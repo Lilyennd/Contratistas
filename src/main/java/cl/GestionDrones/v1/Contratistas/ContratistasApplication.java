56785db9ea7a1712a83382fba4af68f7c1760609
Lilyennd <victoriariffo21@gmail.com>
2026-05-22 11:36:14 -0400

    inicio

---
+++ src/main/java/cl/GestionDrones/v1/Contratistas/ContratistasApplication.java
@@ -0,0 +1,13 @@
+package cl.GestionDrones.v1.Contratistas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratistasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContratistasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/GestionDrones/v1/Contratistas/ContratistasApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.GestionDrones.v1.Contratistas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratistasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
